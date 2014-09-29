2f88da48d920767555ee9391461b49bc91780613
golivia <olivia@olivia.local>
2014-09-28 21:21:55 -0700

    panning successful, starting color assignments

--- mailboxApp-om/MailViewController.swift
+++ mailboxApp-om/MailViewController.swift
@@ -17,11 +17,19 @@ class MailViewController: UIViewController {
     @IBOutlet weak var messageView: UIImageView!
     @IBOutlet weak var feedView: UIImageView!
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var archiveIcon: UIImageView!
+    @IBOutlet weak var deleteIcon: UIImageView!
+    @IBOutlet weak var laterIcon: UIImageView!
+    @IBOutlet weak var listIcon: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.contentSize = CGSizeMake(320, 1438)
+        archiveIcon.alpha = 0
+        deleteIcon.alpha = 0
+        laterIcon.alpha = 0
+        listIcon.alpha = 0
         // Do any additional setup after loading the view.
     }
 
@@ -30,8 +38,28 @@ class MailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onPanMessage(gestureRecognizer: UIPanGestureRecognizer) {
+    @IBAction func onPanContainer(gestureRecognizer: UIPanGestureRecognizer) {
         
+        var location = gestureRecognizer.locationInView(view)
+        var translation = gestureRecognizer.translationInView(view)
+        var velocity = gestureRecognizer.velocityInView(view)
+        var imageCenter = messageView.center
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            imageCenter = messageView.center
+        } else if gestureRecognizer.state == UIGestureRecognizerState.Changed {
+            messageView.center.x = translation.x + 160
+            if velocity.x > 0 {
+                containerView.backgroundColor = UIColor.greenColor()
+            } else if velocity.x < 0 {
+                containerView.backgroundColor = UIColor.yellowColor()
+            } else {
+                
+            }
+        } else if gestureRecognizer.state == UIGestureRecognizerState.Ended {
+            
+        }
+
     }
 
     /*
